c68c19fda017d15399ae9c03ea0b5c9e24c8bb80
maxwilliamchao <maxwilliamchao@gmail.com>
2017-11-30 08:17:23 -0800

    wrapped request in a function and gave it a route

--- server.js
+++ server.js
@@ -37,25 +37,28 @@ app.get("/", function(req, res) {
   res.render('index')
 });
 
-request('http://www.nytimes.com', function(error, response, html){
-  var $ = cheerio.load(html);
+app.get("/nytScrape", function(req, res) {
+  var nytScrape = function() {
+    request('http://www.nytimes.com', function(error, response, html){
+      var $ = cheerio.load(html);
 
-  var results = [];
+      var results = [];
 
-  $("h2.story-heading").each(function(i, element) {
+      $("h2.story-heading").each(function(i, element) {
 
-    var link = $(element).children().attr("href");
-    var title = $(element).children().text();
+        var link = $(element).children().attr("href");
+        var title = $(element).children().text();
 
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+          title: title,
+          link: link
+        });
+      });
+      console.log(results);
     });
-  });
-  console.log(results);
+  };
 });
-
 // app.get("/articles", function(req, res) {
 //   res.render('#')
 // )};
